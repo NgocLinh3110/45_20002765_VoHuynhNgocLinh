a534f5e440b235ff16d31797a42fbf9cac5b4082
NgocLinh3110 <linhvo31102002@gmail.com>
2023-10-27 08:40:17 +0700

    render

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Screen01 from './Screen01';
@@ -13,7 +13,7 @@ const App = () => {
     return ( <
         NavigationContainer >
         <
-        Stack.Navigator initialRouteName = 'Screen01'
+        Stack.Navigator initialRouteName = 'Screen02'
         screenOptions = {
             { headerShown: false } } >
         <
@@ -22,11 +22,164 @@ const App = () => {
         /> <
         Stack.Screen name = 'Screen02'
         component = { Screen02 }
-        /> <
+        options = {
+            {
+                headerTitle: 'Shops Near Me',
+                headerTitleStyle: {
+                    margin: 0
+                },
+                headerRight: () => {
+                    return ( <
+                        View style = {
+                            {
+                                marginRight: 12,
+
+                            }
+                        } >
+                        <
+                        Image style = {
+                            {
+                                width: '24',
+                                height: '24'
+                            }
+                        }
+                        source = { require('./pic/Image 177.png') } >
+                        < /Image>
+
+                        <
+                        /View>
+                    )
+                }
+            }
+        }
+        />
+
+        <
         /Stack.Navigator>
 
         <
         /NavigationContainer>
+    );
+}
+export default function Screen01({ navigation }) {
+    const navigationScreen = useNavigation();
+    const navigationToScreen02 = () => {
+        navigationScreen.navigate('Screen02')
+    }
+    return ( <
+        View style = { styles.container } >
+        <
+        View style = { styles.style1 } >
+        <
+        Text style = {
+            {
+                fontSize: '23px',
+                fontWeight: 'bold'
+            }
+        } > Welcome to Cafe world < /Text> <
+        /View> <
+        View style = { styles.style2 } >
+        <
+        Image style = {
+            {
+                height: '80px',
+                width: '200px',
+                borderRadius: '10px',
+                marginBottom: '30px'
+            }
+        }
+        source = { require('./pic/Image (5).png') } >
+        < /Image> <
+        Image style = {
+            {
+                height: '80px',
+                width: '200px',
+                borderRadius: '10px',
+                marginBottom: '30px'
+            }
+        }
+        source = { require('./pic/Image (5).png') } >
+        < /Image> <
+        Image style = {
+            {
+                height: '80px',
+                width: '200px',
+                borderRadius: '10px'
+            }
+        }
+        source = { require('./pic/Image (5).png') } >
+        < /Image> <
+        /View> <
+        View style = { styles.style3 } >
+        <
+        Pressable style = {
+            {
+                width: '200px',
+                height: '50px',
+                backgroundColor: 'blue',
+                borderRadius: '10px',
+            }
+        }
+
+        onPress = { navigationToScreen02 } >
+        <
+        Text style = {
+            {
+                color: 'white',
+                marginLeft: '50px',
+                marginTop: '12px'
+            }
+        } > GET STARTED < /Text> <
+        /Pressable> <
+        /View> <
+        /View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'grey 50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    style1: {
+        flex: 1,
+        backgroundColor: 'grey 50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    style2: {
+
+        flex: 2,
+        backgroundColor: 'grey 50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    style3: {
+
+        flex: 1,
+        backgroundColor: 'grey 50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+function Screen02({ navigation }) {
+    const navigationScreen = useNavigation();
+    const navigationToScreen03 = () => {
+        navigationScreen.navigate('Screen03')
+    }
+    return ( <
+        View style = { styles.container } >
+        <
+        Text style = {
+            {
+                fontSize: '23px',
+                fontWeight: 'bold'
+            }
+        } > Welcome to Cafe world < /Text> <
+        /View>
     );
 }
 export default App;
--- Screen02.js
+++ Screen02.js
@@ -1,19 +1,149 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, Pressable, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import App from './App';
 
 export default function Screen02({ navigation }) {
-
+    const navigationScreen = useNavigation();
+    const navigationToScreen03 = () => {
+        navigationScreen.navigate('Screen03')
+    }
+    const data = [{
+            name: 'Kitanda Espresso & Acai-U District',
+            type: 'Accepting Orders',
+            waitTime: '5-10 minutes',
+            address: '1121 NE 45 St',
+            img: require('./pic/Image (6).png'),
+            typePic: require('./pic/tick.png'),
+            timePic: require('./pic/Image 191.png'),
+            addressPic: require('./pic/location.png')
+        },
+        {
+            name: 'Kitanda Espresso & Acai-U District',
+            type: 'Tempory Unavailable',
+            waitTime: '10-15 minutes',
+            address: '1121 NE 45 St',
+            img: require('./pic/Image (6).png'),
+            typePic: require('./pic/material-symbols_lock.png'),
+            timePic: require('./pic/Image 191.png'),
+            addressPic: require('./pic/location.png')
+        },
+        {
+            name: 'Kitanda Espresso & Acai-U District',
+            type: 'Accepting Orders',
+            waitTime: '5-10 minutes',
+            address: '1121 NE 45 St',
+            img: require('./pic/Image (6).png'),
+            typePic: require('./pic/tick.png'),
+            timePic: require('./pic/Image 191.png'),
+            addressPic: require('./pic/location.png')
+        },
+        {
+            name: 'Kitanda Espresso & Acai-U District',
+            type: 'Accepting Orders',
+            waitTime: '5-10 minutes',
+            address: '1121 NE 45 St',
+            img: require('./pic/Image (6).png'),
+            typePic: require('./pic/tick.png'),
+            timePic: require('./pic/Image 191.png'),
+            addressPic: require('./pic/location.png')
+        },
+    ];
     return ( <
         View style = { styles.container } >
         <
-        Text style = {
-            {
-                fontSize: '23px',
-                fontWeight: 'bold'
-            }
-        } > Welcome to Cafe world < /Text> <
+        FlatList data = { data }
+        renderItem = {
+            ({ item }) => ( <
+                Pressable style = {
+                    {
+                        width: '90%',
+                        height: '80px'
+                    }
+                }
+                onPress = {
+                    () => navigationToScreen03(item) } >
+                <
+                Image style = {
+                    {
+                        width: '90%',
+                        height: '50px',
+                        borderRadius: '10px'
+                    }
+                }
+                source = { require('./pic/Image (6).png') } >
+                < /Image> <
+                View style = {
+                    {
+                        flexDirection: 'row',
+                        width: '100%',
+                        height: '20px',
+                        gap: '5px'
+                    }
+                } >
+                <
+                Image style = {
+                    {
+                        height: '20px',
+                        width: '20px',
+                    }
+                }
+                source = { item.typePic } > < /Image> <
+                Text style = {
+                    {
+                        color: 'green 80%',
+                        fontSize: '15px',
+                        gap: '10px'
+                    }
+                } > { item.type } < /Text>
+
+                <
+                Image style = {
+                    {
+                        height: '20px',
+                        width: '20px',
+                    }
+                }
+                source = { item.timePic } > < /Image> <
+                Text style = {
+                    {
+                        color: 'green 80%',
+                        fontSize: '15px',
+                        gap: '10px'
+                    }
+                } > { item.waitTime } < /Text>
+
+
+                <
+                Image style = {
+                    {
+                        height: '20px',
+                        width: '20px',
+                    }
+                }
+                source = { item.addressPic } > < /Image> <
+                /View> <
+                Text style = {
+                    {
+                        fontWeight: 'bold',
+                        fontSize: '20px'
+                    }
+                }
+                numberOfLines = { 1 } >
+                { item.name } < /Text> <
+                Text style = {
+                    {
+                        fontSize: '15px',
+                        color: 'grey 70%'
+                    }
+                } > { item.address } < /Text> <
+                /Pressable>
+            )
+        } >
+
+        <
+        /FlatList> <
         /View>
     );
 }
